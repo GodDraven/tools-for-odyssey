6e1c9d40bd702d9c19334e26fe875e73fc719fa3
GodDraven <1766265384@qq.com>
2024-05-06 06:01:56 +0000

    compile the smallbank smart contract

--- script/public_tx_web3.js
+++ script/public_tx_web3.js
@@ -2,10 +2,10 @@ const path = require("path");
 const fs = require("fs-extra");
 var {Web3} = require("web3");
 // use the existing Member1 account address or make a new account
-const address = "0xce3a70d3d207ea92c54b4d6e59d07b8e6d4caf96";
+const address = "0x8adee3087359ef8435aab2c919d37af29f7e1dc4";
 
 // read in the contracts
-const contractJsonPath = path.resolve(__dirname, "SimpleStorage.json");
+const contractJsonPath = path.resolve(__dirname, "../contract/smallbank.json");
 const contractJson = JSON.parse(fs.readFileSync(contractJsonPath));
 const contractAbi = contractJson.abi;
 const contractByteCode = contractJson.evm.bytecode.object;
@@ -21,7 +21,7 @@ async function createContract(
     const contractInstance = new web3.eth.Contract(contractAbi);
     const ci = await contractInstance
         .deploy({ data: "0x" + contractByteCode, arguments: [contractInit] })
-        .send({ from: fromAddress, gasLimit: "0x300000" })
+        .send({ from: fromAddress, gas: "300000", gasPrice: "0x0" })
         .on("transactionHash", function (hash) {
             console.log("The transaction hash is: " + hash);
         });
